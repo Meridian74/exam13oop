e493d26741e604126d392b71b70b695099e079a0
meridian74 <meridian@freemail.hu>
2021-03-24 15:38:54 +0100

    almost ready (done) Only one test is reamined.

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,53 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
+    private List<Animal> animals;
+
+    public Farm(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void feedAnimals() {
+        for (Animal animal : animals) {
+            animal.feed();
+        }
+    }
+
+    public boolean isEmpty() {
+        if (animals == null || animals.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void butcher(Butcher butcher) {
+        for (Animal animal : animals) {
+            if (butcher.canButcher(animal)) {
+                animals.remove(animal);
+            }
+        }
+    }
+
+    public List<String> getStatus() {
+        List<String> animalStatus = new ArrayList<>();
+        for (Animal animal : animals) {
+            String textElement = "There is a " + animal.getSize() + " sized "
+                    + animal.getClass().getSimpleName().toLowerCase() + " in the farm.";
+            animalStatus.add(textElement);
+            System.out.println(textElement);
+        }
+        return animalStatus;
+    }
 
 }
+
--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -1,5 +1,15 @@
 package com.codecool.farm.animal;
 
 public abstract class Animal {
+    private int size = 0;
 
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public abstract void feed();
 }
